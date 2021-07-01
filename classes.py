a95ddf935fd7b4f4dec175ddd3364e57ff7fa3aa
Gaurav Gidwani <gggidwani@gmail.com>
2021-07-01 14:16:47 +0800

    Started working on classes

---
+++ classes.py
@@ -0,0 +1,9 @@
+class Car:
+    """docstring for Car."""
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+tessie = Car('tesla', 'model s', 2019)
